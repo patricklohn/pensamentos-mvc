273294e50005c2369f343059649587e76cf311ef
patricklohn <patricklohn3@gmail.com>
2025-01-11 15:40:58 -0300

    Dando continuidade na estrutura do projeto

--- index.js
+++ index.js
@@ -9,6 +9,55 @@ const port = 3000;
 
 const conn = require('./db/conn')
 
+app.engine('handlebars', exphs.engine())
+app.set('view engine', 'handlebars')
+
+// receber resposta do body
+app.use(
+    express.urlencoded({
+        extended: true
+    })
+)
+
+app.use(express.json())
+
+// session middleware
+app.use(
+    session({
+        name: 'session',
+        secret: "mh]q9xf:#=CYIw95NNL8q)H0c",
+        resave: false, 
+        saveUninitialized: false,
+        store: new FileStore({
+            logFn: function(){},
+            path: require('path').join(require('os').tmpdir(), 'sessions'),
+        }),
+        cookie:{
+            secure: false,
+            maxAge: 360000,
+            expires: new Date(Date.now() + 360000),
+            httpOnly: true,
+        }
+    })
+)
+
+// flash messages 
+app.use(flash())
+
+// public path
+app.use(express.static('public'))
+
+// set session to res
+app.use((req, res, next) => {
+
+    if(req.session.userid){
+        res.locals.session = req.session
+    }
+
+    next()
+
+})
+
 conn.sync().then(()=>{
     app.listen(port)
 }).catch((err) => console.log(err))
