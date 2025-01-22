9fb72aeb3204869f26a7e533560db2e3038495be
patricklohn <patricklohn3@gmail.com>
2025-01-21 23:53:30 -0300

    retorno no dashboard

--- controllers/ToughtsController.js
+++ controllers/ToughtsController.js
@@ -7,7 +7,17 @@ module.exports = class ToughtController {
     }
 
     static async dashboard(req,res){
-        res.render('toughts/dashboard')
+
+        const userUuid = req.session.useruuid
+        const user = await User.findOne({where:{uuid: userUuid,}, include: Tought, plain:true})
+
+        if(!user){
+            res.redirect('/login')
+        }
+
+        const toughts = user.Toughts.map((result)=> result.dataValues)
+
+        res.render('toughts/dashboard', {toughts})
     }
 
     static createTought(req,res){
@@ -25,7 +35,7 @@ module.exports = class ToughtController {
             await Tought.create(tought)
 
             req.flash('message', 'pensamento criado com sucesso!')
-            
+
             req.session.save(() => {
                 res.redirect('/toughts/dashboard')
             })    
