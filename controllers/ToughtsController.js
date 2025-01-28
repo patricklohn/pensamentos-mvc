533b5506f3034729de4796a79eb8bf5d94624cbb
patricklohn <patricklohn3@gmail.com>
2025-01-28 20:42:00 -0300

    filtro search

--- controllers/ToughtsController.js
+++ controllers/ToughtsController.js
@@ -1,15 +1,26 @@
 const Tought = require('../models/Tought')
 const User = require('../models/User')
 
+const { Op } = require('sequelize')
+
 module.exports = class ToughtController {
     static async showToughts(req, res){
 
+        let search= '';
+
+        if(req.query.search){
+            search = req.query.search
+        }
+
         const toughtsData = await Tought.findAll({
             include: User,
+            where: {
+                title: {[Op.like]:`%${search}%`},
+            }
         })
         const toughts = toughtsData.map((result)=> result.get({plain: true}))
 
-        res.render('toughts/home', {toughts})
+        res.render('toughts/home', {toughts , search})
     }
 
     static async dashboard(req,res){
