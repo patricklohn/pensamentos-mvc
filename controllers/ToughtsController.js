1413cc951f6dddc1dc8420ee9e3af7f24d83d4e8
patricklohn <patricklohn3@gmail.com>
2025-01-28 22:42:42 -0300

    Criação de order

--- controllers/ToughtsController.js
+++ controllers/ToughtsController.js
@@ -12,15 +12,30 @@ module.exports = class ToughtController {
             search = req.query.search
         }
 
+        let order = 'DESC'
+
+        if(req.query.order === 'old'){
+            order = 'ASC'
+        } else {
+            order = 'DESC'
+        }
+
         const toughtsData = await Tought.findAll({
             include: User,
             where: {
                 title: {[Op.like]:`%${search}%`},
-            }
+            },
+            order: [['createdAt', order]]
         })
-        const toughts = toughtsData.map((result)=> result.get({plain: true}))
 
-        res.render('toughts/home', {toughts , search})
+        const toughts = toughtsData.map((result)=> result.get({plain: true}))
+        let toughtsQty = toughts.length;
+
+        if(toughtsQty === 0){
+            toughtsQty = false
+        }
+
+        res.render('toughts/home', {toughts , search, toughtsQty})
     }
 
     static async dashboard(req,res){
